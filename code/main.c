792dcd613fe75d39abe5002fed0cb59a954df275
xDwake <gabi.alex.sil2002@gmail.com>
2021-03-19 12:03:14 +0000

    codigo visto nos videos

---
+++ code/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "parser.h"
+
+int main (){
+    char line [10240];
+
+    assert( fgets(line, 10240,stdin) != NULL);
+    assert( line[strlen(line)-1] == '\n');
+    
+    parser(line);
+    return 0;
+}
